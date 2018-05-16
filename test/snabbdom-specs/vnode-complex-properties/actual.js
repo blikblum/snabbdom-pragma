e4fe1d4127fdf5c43deeeafab459ecae5d37059e
Luiz Américo <luizamericop@gmail.com>
2018-05-16 00:36:43 -0300

    Add test for complex value

---
+++ test/snabbdom-specs/vnode-complex-properties/actual.js
@@ -0,0 +1,4 @@
+
+export default (createElement) => {
+  return createElement('div', { objectprop: {type: 'x', value: 3} })
+}
---
+++ test/snabbdom-specs/vnode-complex-properties/expected.js
@@ -0,0 +1,4 @@
+
+export default (h) => {
+  return h('div', { props: { objectprop: {type: 'x', value: 3} } }, [])
+}
