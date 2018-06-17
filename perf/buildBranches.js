ea584a357077385f6cd5319bc1fdfaeaf95cc7d8
Luiz Américo <luizamericop@gmail.com>
2018-06-17 19:37:10 -0300

    Compile perf builds directly to target file

--- perf/buildBranches.js
+++ perf/buildBranches.js
@@ -1,12 +1,13 @@
-const execSync = require('child_process').execSync
-const fs = require('fs');
-const simpleGit = require('simple-git')();
+/* eslint unicorn/no-process-exit: 0 */
 
-let baseBranch =  process.argv[2] || 'develop'
+const execSync = require('child_process').execSync
+const fs = require('fs')
+const simpleGit = require('simple-git')()
+
+const baseBranch = process.argv[2] || 'develop'
 
 function buildSnabbdom(suffix) {
-  execSync('npm run build', {stdio: [0, 1, 2]});
-  fs.writeFileSync(`perf/snnabdom-${suffix}.js`, fs.readFileSync('dist/index.js', 'utf-8'), 'utf-8')
+  execSync(`npm run build -- --file perf/snabbdom-${suffix}.js`, { stdio: [0, 1, 2] })  
 }
 
 simpleGit.status((err, status) => {
@@ -21,18 +22,18 @@ simpleGit.status((err, status) => {
     } else {
       console.log('Building ' + currentBranch + ' branch')
       buildSnabbdom('new')
-      simpleGit.checkout(baseBranch, (err, data) => {
+      simpleGit.checkout(baseBranch, (err) => {
         if (err) {
           console.error('Error checkout ' + baseBranch)
         } else {
           console.log('Building ' + baseBranch + ' branch')
-          buildSnabbdom('base')          
+          buildSnabbdom('base')
         }
         simpleGit.checkout(currentBranch)
         if (err) {
           process.exit(1)
         }
       })
-    }  
+    }
   }
 })
